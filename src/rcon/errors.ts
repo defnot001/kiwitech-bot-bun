ce3e496c89614a635020428718afa46c9580d00c
defnot001 <marc.miller994@gmail.com>
2023-12-20 18:40:54 +0100

    added custom rcon errors

---
+++ src/rcon/errors.ts
@@ -0,0 +1,16 @@
+export class RconError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'RconError';
+  }
+}
+
+export const ERROR_MESSAGES = {
+  NOT_CONNECTED: 'Cannot send command because the client is not connected',
+  ALREADY_CONNECTED:
+    'Already connected. Please disconnect before trying to connect again.',
+  AUTH_FAILED: 'Client authentification failed',
+  END_CALLED_TWICE: 'End called twice',
+  SOCKET_NOT_INITIALIZED: 'Socket is not initialized',
+  CONNECTION_FAILED: 'Failed to connect to the server.',
+};
