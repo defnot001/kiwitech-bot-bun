f25a34ea577e87bfbcd7767dc053a6cf5ee5348d
defnot001 <marc.miller994@gmail.com>
2023-12-29 18:16:10 +0100

    added whitelist command

---
+++ src/commands/whitelist.ts
@@ -0,0 +1,175 @@
+import { ApplicationCommandOptionType, inlineCode } from 'discord.js';
+import { Command } from '../handler/classes/Command';
+import { KoalaEmbedBuilder } from '../classes/KoalaEmbedBuilder';
+import { config, ServerChoice } from '../config';
+import { getServerChoices } from '../util/helpers';
+import { handleInteractionError } from '../util/loggers';
+import RCONUtil from '../util/rcon';
+
+export default new Command({
+  name: 'whitelist',
+  description: 'Get information about the whitelist & add/remove users.',
+  options: [
+    {
+      name: 'add',
+      description: 'Adds a player to the whitelist on all minecraft servers.',
+      type: ApplicationCommandOptionType.Subcommand,
+      options: [
+        {
+          name: 'ign',
+          description: `The player's in-game name.`,
+          type: ApplicationCommandOptionType.String,
+          required: true,
+        },
+      ],
+    },
+    {
+      name: 'remove',
+      description:
+        'Removes a player from the whitelist on all minecraft servers.',
+      type: ApplicationCommandOptionType.Subcommand,
+      options: [
+        {
+          name: 'ign',
+          description: `The player's in-game name.`,
+          type: ApplicationCommandOptionType.String,
+          required: true,
+          autocomplete: true,
+        },
+      ],
+    },
+    {
+      name: 'list',
+      description: 'Returns the whitelist of the specified server in an embed.',
+      type: ApplicationCommandOptionType.Subcommand,
+      options: [
+        {
+          name: 'server',
+          description: `Specify a server.`,
+          type: ApplicationCommandOptionType.String,
+          choices: [...getServerChoices()],
+          required: true,
+        },
+      ],
+    },
+  ],
+  execute: async ({ interaction, args }) => {
+    await interaction.deferReply();
+
+    const subcommand = args.getSubcommand();
+
+    if (!subcommand) {
+      return interaction.editReply('This subcommand does not exist!');
+    }
+
+    if (!interaction.guild) {
+      return interaction.reply('This command can only be used in a guild.');
+    }
+
+    try {
+      if (subcommand === 'list') {
+        const choice = args.getString('server', true) as ServerChoice;
+
+        if (!choice) {
+          return interaction.editReply('Please specify a server!');
+        }
+
+        const response = await getWhitelist(choice);
+
+        const whitelist = !response
+          ? `There are no whitelisted players on ${choice}!`
+          : response.map((ign) => escapeMarkdown(ign)).join('\n');
+
+        const whitelistEmbed = new KoalaEmbedBuilder(interaction.user, {
+          title: `${choice.toUpperCase()} Whitelist`,
+          description: whitelist,
+        });
+
+        const iconURL = interaction.guild.iconURL();
+
+        if (iconURL) {
+          whitelistEmbed.setThumbnail(iconURL);
+        }
+
+        return interaction.editReply({ embeds: [whitelistEmbed] });
+      } else {
+        const ign = args.getString('ign');
+
+        if (!ign) {
+          return interaction.editReply('Please provide an in-game name!');
+        }
+
+        const servers = Object.keys(config.mcConfig);
+
+        const whitelistCheck: [string, string][] = [];
+        const opCheck: [string, string][] = [];
+
+        for await (const server of servers) {
+          const whitelistCommand = `whitelist ${subcommand} ${ign}`;
+
+          const whitelist = await RCONUtil.runSingleCommand(
+            server as ServerChoice,
+            whitelistCommand,
+          );
+
+          whitelistCheck.push([server, whitelist]);
+
+          if (config.mcConfig[server as ServerChoice].operator) {
+            const action = subcommand === 'add' ? 'op' : 'deop';
+            const opCommand = `${action} ${ign}`;
+
+            const op = await RCONUtil.runSingleCommand(
+              server as ServerChoice,
+              opCommand,
+            );
+
+            opCheck.push([server, op]);
+          }
+        }
+        const successMessage =
+          subcommand === 'add'
+            ? `Successfully added ${inlineCode(ign)} to the whitelist on ${
+                whitelistCheck.length
+              } servers.\nSuccessfully made ${inlineCode(ign)} an operator on ${
+                opCheck.length
+              } servers.`
+            : `Successfully removed ${inlineCode(ign)} from the whitelist on ${
+                whitelistCheck.length
+              } servers.\nSuccessfully removed ${inlineCode(
+                ign,
+              )} as an operator on ${opCheck.length} servers.`;
+
+        return interaction.editReply(successMessage);
+      }
+    } catch (err) {
+      return handleInteractionError({
+        interaction,
+        err,
+        message: `There was an error trying to execute the whitlist ${subcommand} command!`,
+      });
+    }
+  },
+});
+
+function escapeMarkdown(text: string): string {
+  const unescaped = text.replace(/\\(\*|_|`|~|\\)/g, '$1');
+  return unescaped.replace(/(\*|_|`|~|\\)/g, '\\$1');
+}
+
+async function getWhitelist(server: ServerChoice) {
+  const response = await RCONUtil.runSingleCommand(server, 'whitelist list');
+
+  if (response === 'There are no whitelisted players') {
+    return [];
+  }
+
+  const splitResponse = response.split(': ')[1];
+
+  if (!splitResponse) {
+    throw new Error('Failed to parse the response correctly!');
+  }
+
+  return splitResponse
+    .split(', ')
+    .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+}
