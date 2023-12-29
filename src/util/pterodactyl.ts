276245b63e3658302315c2662e91e52f40e31dc2
defnot001 <marc.miller994@gmail.com>
2023-12-29 15:46:30 +0100

    added function to get contents of serverProperties file

--- src/util/pterodactyl.ts
+++ src/util/pterodactyl.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { PteroClient } from 'ptero-client';
 import { config, ServerChoice } from '../config';
 
@@ -45,4 +44,80 @@ export async function getServerState(serverChoice: ServerChoice) {
   );
 
   return serverStats.current_state;
+}
+
+export async function getServerProperties(serverChoice: ServerChoice) {
+  const { serverId } = config.mcConfig[serverChoice];
+  const fileContent = (await ptero.files.getContent(
+    serverId,
+    'server.properties',
+  )) as string;
+
+  return parseServerProperties(fileContent);
+}
+
+type ServerProperties = {
+  seed: string | undefined;
+  gamemode: 'survival' | 'creative' | undefined;
+  motd: string | undefined;
+  difficulty: 'hard' | 'normal' | 'easy' | 'peaceful' | undefined;
+  maxPlayers: number | undefined;
+  viewDistance: number | undefined;
+  simulationDistance: number | undefined;
+  hardcore: boolean | undefined;
+  whitelist: boolean | undefined;
+  levelType: string | undefined;
+};
+
+function parseServerProperties(propertiesText: string): ServerProperties {
+  const lines = propertiesText.split('\n');
+  const properties: Partial<ServerProperties> = {};
+
+  for (const line of lines) {
+    if (line.startsWith('#') || !line.includes('=')) {
+      continue;
+    }
+
+    const [key, value] = line.split('=');
+
+    switch (key) {
+      case 'level-seed':
+        properties.seed = value;
+        break;
+      case 'gamemode':
+        properties.gamemode = value as 'survival' | 'creative' | undefined;
+        break;
+      case 'motd':
+        properties.motd = value;
+        break;
+      case 'difficulty':
+        properties.difficulty = value as
+          | 'hard'
+          | 'normal'
+          | 'easy'
+          | 'peaceful'
+          | undefined;
+        break;
+      case 'max-players':
+        properties.maxPlayers = parseInt(value!);
+        break;
+      case 'view-distance':
+        properties.viewDistance = parseInt(value!);
+        break;
+      case 'simulation-distance':
+        properties.simulationDistance = parseInt(value!);
+        break;
+      case 'hardcore':
+        properties.hardcore = value === 'true';
+        break;
+      case 'white-list':
+        properties.whitelist = value === 'true';
+        break;
+      case 'level-type':
+        properties.levelType = value;
+        break;
+    }
+  }
+
+  return properties as ServerProperties;
 }
