6770c5d4cc4c4db6fc31a28126be53dd5e1662e9
defnot001 <marc.miller994@gmail.com>
2024-01-04 00:02:34 +0100

    implemented list, delete and accept subcommands

--- src/commands/application.ts
+++ src/commands/application.ts
@@ -1,8 +1,20 @@
-import { ApplicationCommandOptionType, EmbedBuilder, User, userMention } from 'discord.js';
+import {
+  ApplicationCommandOptionType,
+  EmbedBuilder,
+  GuildChannelManager,
+  Snowflake,
+  User,
+  time,
+  userMention,
+} from 'discord.js';
 import { Command } from '../handler/classes/Command';
 import {
+  closeApplication,
+  deleteApplication,
   getApplicationFromID,
   getLatestApplicationFromMember,
+  getLatestApplications,
+  getLatestOpenApplications,
   updateApplication,
 } from '../util/prisma';
 import { ERROR_MESSAGES } from '../util/constants';
@@ -10,6 +22,8 @@ import { getTextChannelFromID, handleInteractionError } from '../util/loggers';
 import { getApplicationEmbeds, notifyUserApplicationRecieved } from '../util/application';
 import { getEmojis } from '../util/components';
 import { config } from '../config';
+import { KoalaEmbedBuilder } from '../classes/KoalaEmbedBuilder';
+import { getTrialWelcomeMessage } from '../assets/welcomeMessage';
 
 type ApplicationSubcommand =
   | 'display_latest'
@@ -95,6 +109,50 @@ export default new Command({
         },
       ],
     },
+    {
+      name: 'list',
+      description: 'List the latest applications.',
+      type: ApplicationCommandOptionType.Subcommand,
+      options: [
+        {
+          name: 'type',
+          description:
+            'Wether to list open applications only or all applications. Defaults to open.',
+          type: ApplicationCommandOptionType.String,
+          required: false,
+          choices: [
+            {
+              name: 'open',
+              value: 'open',
+            },
+            {
+              name: 'all',
+              value: 'all',
+            },
+          ],
+        },
+        {
+          name: 'amount',
+          description:
+            'The amount of applications to list. Defaults to 20, which is also the maximum.',
+          type: ApplicationCommandOptionType.Integer,
+          required: false,
+        },
+      ],
+    },
+    {
+      name: 'delete',
+      description: 'Delete an application.',
+      type: ApplicationCommandOptionType.Subcommand,
+      options: [
+        {
+          name: 'application_id',
+          description: 'The ID of the application.',
+          type: ApplicationCommandOptionType.Integer,
+          required: true,
+        },
+      ],
+    },
   ],
   execute: async ({ interaction, args }) => {
     await interaction.deferReply();
@@ -119,10 +177,19 @@ export default new Command({
       const applicationID = args.getInteger('application_id', true);
       const messageID = args.getString('message_id', true);
 
+      const application = await getApplicationFromID(applicationID);
+
+      if (!application) {
+        return interaction.editReply(`Application with ID ${applicationID} not found.`);
+      }
+
       try {
-        const { content } = await getApplicationFromID(applicationID);
-        content.discordName = targetUser.globalName ?? targetUser.username;
-        const updatedApplication = await updateApplication(applicationID, targetUser, content);
+        application.content.discordName = targetUser.globalName ?? targetUser.username;
+        const updatedApplication = await updateApplication(
+          applicationID,
+          targetUser,
+          application.content,
+        );
 
         const applicationChannel = await getTextChannelFromID(interaction.guild, 'application');
         const message = await applicationChannel.messages.fetch(messageID);
@@ -256,15 +323,102 @@ export default new Command({
     }
 
     if (subcommand === 'list') {
-      return interaction.editReply('Not implemented yet.');
+      const listType = (args.getString('type', false) ?? 'open') as 'open' | 'all';
+      const amount = args.getInteger('amount') ?? 20;
+
+      const applications =
+        listType === 'open'
+          ? await getLatestOpenApplications(amount)
+          : await getLatestApplications(amount);
+
+      const display = applications.map((a) => {
+        return `Application ID: ${a.id}\nApplicant: ${a.content.discordName}\nTime: ${time(
+          a.createdAt,
+          'D',
+        )}`;
+      });
+
+      const applicationListEmbed = new KoalaEmbedBuilder(interaction.user, {
+        title: `Latest${listType === 'open' ? 'open ' : ' '}applications`,
+        description: display.join('\n\n'),
+      });
+
+      return interaction.editReply({ embeds: [applicationListEmbed] });
     }
 
     if (subcommand === 'delete') {
-      return interaction.editReply('Not implemented yet.');
+      const applicationID = args.getInteger('application_id', true);
+
+      try {
+        await deleteApplication(applicationID);
+        await interaction.editReply(`Successfully deleted application ID ${applicationID}.`);
+      } catch {
+        await interaction.editReply(`Failed to delete application ID ${applicationID}.`);
+      }
     }
 
     if (subcommand === 'accept') {
-      return interaction.editReply('Not implemented yet.');
+      const applicationID = args.getInteger('application_id', true);
+      const application = await getApplicationFromID(applicationID);
+
+      if (!application) {
+        return interaction.editReply(`Application with ID ${applicationID} not found.`);
+      }
+
+      if (!application.discordID) {
+        return interaction.editReply(
+          `Application with ID ${applicationID} does not have a linked user.`,
+        );
+      }
+
+      try {
+        const targetUser = await interaction.client.users.fetch(application.discordID);
+        const applicationChannel = await getApplicationChannel(
+          interaction.guild.channels,
+          targetUser,
+        );
+
+        if (!applicationChannel) {
+          return interaction.editReply(
+            `Application channel for ${application.discordID} not found.`,
+          );
+        }
+
+        if (!applicationChannel.isTextBased()) {
+          return interaction.editReply(
+            `Application channel for ${application.discordID} is not a text channel.`,
+          );
+        }
+
+        await applicationChannel.send(getAcceptMessage(application.discordID));
+        await closeApplication(applicationID);
+
+        const { kiwi } = getEmojis(interaction.client);
+
+        const trialEmbed = new KoalaEmbedBuilder(interaction.user, {
+          title: `${kiwi}  Welcome to ${interaction.guild.name} ${targetUser.username}!  ${kiwi}`,
+          thumbnail: {
+            url: targetUser.displayAvatarURL(),
+          },
+          fields: getTrialWelcomeMessage(interaction.client),
+        });
+
+        const memberGeneralChannel = await getTextChannelFromID(interaction.guild, 'memberGeneral');
+
+        const message = await memberGeneralChannel.send({
+          content: userMention(targetUser.id),
+          embeds: [trialEmbed],
+        });
+
+        await message.edit({ content: '\u200b' });
+        return interaction.editReply(`Successfully accepted application ID ${applicationID}.`);
+      } catch (err) {
+        handleInteractionError({
+          interaction,
+          err,
+          message: `Failed to accept application ID ${applicationID}.`,
+        });
+      }
     }
 
     if (subcommand === 'deny') {
@@ -279,4 +433,16 @@ function getWelcomeMessage(user: User) {
   return `Welcome to your application channel ${userMention(
     user.id,
   )}! You can use this channel to communicate with the members about your application and share more images and information. Don't hesistate to ask questions! There is a vote active on our application where every member can vote on your application.`;
+}
+
+function getAcceptMessage(userID: Snowflake) {
+  return `We are happy to inform you that your application has been accepted, ${userMention(
+    userID,
+  )}! Welcome to the community! ${config.emoji.froghypers}`;
+}
+
+async function getApplicationChannel(channelManager: GuildChannelManager, user: User) {
+  await channelManager.fetch();
+  const channel = await channelManager.cache.get(`${user.username}-application`);
+  return channel;
 }
