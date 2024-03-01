cc796b38f63b7e110725641f557cdf4fb9cc7674
defnot001 <marc.miller994@gmail.com>
2024-03-01 19:41:07 +0100

    change api to be an abstract class and add method to get profiles

--- src/util/mojang.ts
+++ src/util/mojang.ts
@@ -1,12 +1,5 @@
-import { Client, Guild } from 'discord.js';
-import { logErrorToBotLogChannel } from './loggers';
-
-export async function getMojangUUID(
-  username: string,
-  guild: Guild,
-  client: Client,
-): Promise<{ id: string; name: string } | undefined> {
-  try {
+export default abstract class MojangAPI {
+  static async getUUID(username: string) {
     const res = await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`);
 
     if (!res.ok) {
@@ -16,53 +9,61 @@ export async function getMojangUUID(
     const data = (await res.json()) as { id: string; name: string };
 
     return {
-      id: formatMojangUUID(data.id),
+      id: this.formatMojangUUID(data.id),
       name: data.name,
     };
-  } catch (err) {
-    await logErrorToBotLogChannel({
-      client,
-      guild,
-      message: `Failed to get the uuid for ${username}`,
-      error: err,
+  }
+
+  static async getUUIDs(usernames: string[]) {
+    if (usernames.length > 10) {
+      throw new Error('Too many usernames');
+    }
+
+    const response = await fetch(
+      'https://api.minecraftservices.com/minecraft/profile/lookup/bulk/byname',
+      {
+        method: 'POST',
+        body: JSON.stringify(usernames),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+
+    const data = (await response.json()) as Array<{ id: string; name: string }>;
+
+    return data.map((user) => {
+      return {
+        id: this.formatMojangUUID(user.id),
+        name: user.name,
+      };
     });
-
-    return;
-  }
-}
-
-export async function getMultipleUUIDs(usernames: string[]) {
-  if (usernames.length < 1) {
-    throw new Error('No usernames provided');
   }
 
-  if (usernames.length > 10) {
-    throw new Error('Too many usernames');
-  }
+  static async getProfile(uuid: string) {
+    const res = await fetch(`https://sessionserver.mojang.com/session/minecraft/profile/${uuid}`);
 
-  const response = await fetch('https://api.mojang.com/profiles/minecraft', {
-    method: 'POST',
-    body: JSON.stringify(usernames),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+    if (!res.ok) {
+      throw new Error(`${res.status}: ${res.statusText}`);
+    }
 
-  const data = (await response.json()) as Array<{ id: string; name: string }>;
-
-  const mapped = data.map((user) => {
-    return {
-      id: formatMojangUUID(user.id),
-      name: user.name,
+    const data = (await res.json()) as {
+      id: string;
+      name: string;
+      properties: Array<{ name: string; value: string }>;
     };
-  });
 
-  return mapped;
-}
+    return {
+      id: this.formatMojangUUID(data.id),
+      name: data.name,
+      properties: data.properties,
+    };
+  }
 
-function formatMojangUUID(uuid: string) {
-  return `${uuid.slice(0, 8)}-${uuid.slice(8, 12)}-${uuid.slice(12, 16)}-${uuid.slice(
-    16,
-    20,
-  )}-${uuid.slice(20)}`;
+  private static formatMojangUUID(uuid: string) {
+    return `${uuid.slice(0, 8)}-${uuid.slice(8, 12)}-${uuid.slice(12, 16)}-${uuid.slice(
+      16,
+      20,
+    )}-${uuid.slice(20)}`;
+  }
 }
