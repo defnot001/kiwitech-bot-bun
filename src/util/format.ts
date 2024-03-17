7b41f7942260f79169af0b9476c963a2819e44d0
defnot001 <marc.miller994@gmail.com>
2024-03-17 21:24:56 +0100

    add # to make it more obvious that it's a channel

--- src/util/format.ts
+++ src/util/format.ts
@@ -1,43 +1,43 @@
-import { Guild, GuildMember, TextChannel, User, escapeMarkdown, inlineCode } from "discord.js";
+import { Guild, GuildMember, TextChannel, User, escapeMarkdown, inlineCode } from 'discord.js';
 
 type Displayable = Guild | User | GuildMember | TextChannel;
 
 export function display(obj: Displayable): string {
-  if (obj instanceof Guild) {
-    return `${obj.name} (${obj.id})`;
-  }
+	if (obj instanceof Guild) {
+		return `${obj.name} (${obj.id})`;
+	}
 
-  if (obj instanceof User) {
-    return `${obj.globalName ?? obj.username} (${obj.id})`;
-  }
+	if (obj instanceof User) {
+		return `${obj.globalName ?? obj.username} (${obj.id})`;
+	}
 
-  if (obj instanceof GuildMember) {
-    return `${obj.user.globalName ?? obj.user.username} (${obj.id})`;
-  }
+	if (obj instanceof GuildMember) {
+		return `${obj.user.globalName ?? obj.user.username} (${obj.id})`;
+	}
 
-  if (obj instanceof TextChannel) {
-    return `${obj.name} (${obj.id})`;
-  }
+	if (obj instanceof TextChannel) {
+		return `#${obj.name} (${obj.id})`;
+	}
 
-  return 'Unknown object type.';
+	return 'Unknown object type.';
 }
 
 export function displayFormatted(obj: Displayable): string {
-  if (obj instanceof Guild) {
-    return `${escapeMarkdown(obj.name)} (${inlineCode(obj.id)})`;
-  }
+	if (obj instanceof Guild) {
+		return `${escapeMarkdown(obj.name)} (${inlineCode(obj.id)})`;
+	}
 
-  if (obj instanceof User) {
-    return `${escapeMarkdown(obj.globalName ?? obj.username)} (${inlineCode(obj.id)})`;
-  }
+	if (obj instanceof User) {
+		return `${escapeMarkdown(obj.globalName ?? obj.username)} (${inlineCode(obj.id)})`;
+	}
 
-  if (obj instanceof GuildMember) {
-    return `${escapeMarkdown(obj.user.globalName ?? obj.user.username)} (${inlineCode(obj.id)})`;
-  }
+	if (obj instanceof GuildMember) {
+		return `${escapeMarkdown(obj.user.globalName ?? obj.user.username)} (${inlineCode(obj.id)})`;
+	}
 
-  if (obj instanceof TextChannel) {
-    return `${escapeMarkdown(obj.name)} (${inlineCode(obj.id)})`;
-  }
+	if (obj instanceof TextChannel) {
+		return `${escapeMarkdown(obj.name)} (${inlineCode(obj.id)})`;
+	}
 
-  return 'Unknown object type.';
+	return 'Unknown object type.';
 }
