d5a706e80134827217a372986d5bf1ee1cfb07c6
defnot001 <marc.miller994@gmail.com>
2024-03-22 12:19:05 +0100

    rewrite status command

--- src/commands/status.ts
+++ src/commands/status.ts
@@ -1,11 +1,12 @@
 import { ApplicationCommandOptionType } from 'discord.js';
 import { KoalaEmbedBuilder } from '../classes/KoalaEmbedBuilder';
 import { type ServerChoice, config } from '../config';
+import { BaseKiwiCommandHandler } from '../util/commandhandler';
 import { Command } from '../util/handler/classes/Command';
 import { getServerChoices } from '../util/helpers';
 import { LOGGER } from '../util/logger';
 import MCStatus from '../util/mcstatus';
-import { getServerState } from '../util/pterodactyl';
+import { ptero } from '../util/pterodactyl';
 import RCONUtil from '../util/rcon';
 
 export const status = new Command({
@@ -20,82 +21,275 @@ export const status = new Command({
 			choices: [...getServerChoices()],
 		},
 	],
-	execute: async ({ interaction, args }) => {
+	execute: async ({ interaction, client, args }) => {
 		await interaction.deferReply();
 
-		const server = args.getString('server', true) as ServerChoice;
-
-		if (!server) {
-			return interaction.editReply('Please specify a server!');
+		const handler = new StatusCommandHandler({ interaction, client });
+		if (!(await handler.init())) {
+			return;
 		}
 
-		if (!interaction.guild) {
-			return interaction.reply('This command can only be used in a server!');
-		}
-
-		try {
-			const serverState = await getServerState(server);
-
-			if (serverState !== 'running') {
-				return interaction.editReply(`Server is currently ${serverState}!`);
-			}
-
-			const status = await MCStatus.queryFull(server);
-			const serverMetrics = await getServerMetrics(server);
-
-			const playerlist =
-				serverMetrics.players.playerList.join('\n') || 'There is currently nobody online.';
-
-			const statusEmbed = new KoalaEmbedBuilder(interaction.user, {
-				title: `${interaction.guild.name} ${server.toUpperCase()}`,
-				color: config.embedColors.green,
-				fields: [
-					{ name: 'Status', value: 'Online' },
-					{ name: 'Version', value: `${status.version?.name_clean}` },
-					{
-						name: 'Performance',
-						value: `**${serverMetrics.performance.mspt}** MSPT | **${serverMetrics.performance.tps}** TPS`,
-					},
-					{
-						name: 'Hostile Mobcaps',
-						value: `Overworld: ${serverMetrics.mobcaps.overworld}\nThe Nether: ${serverMetrics.mobcaps.the_nether}\nThe End: ${serverMetrics.mobcaps.the_end}`,
-					},
-					{
-						name: 'Playercount',
-						value: `online: **${serverMetrics.players.count}** | max: **${serverMetrics.players.max}**`,
-					},
-					{
-						name: 'Playerlist',
-						value: playerlist,
-					},
-				],
-			});
-
-			const { mspt } = serverMetrics.performance;
-
-			if (mspt >= 30 && mspt < 40) {
-				statusEmbed.setColor(config.embedColors.yellow);
-			} else if (mspt >= 40 && mspt < 50) {
-				statusEmbed.setColor(config.embedColors.orange);
-			} else if (mspt >= 50) {
-				statusEmbed.setColor(config.embedColors.red);
-			}
-
-			const guildIcon = interaction.guild.iconURL();
-
-			if (guildIcon) {
-				statusEmbed.setThumbnail(guildIcon);
-			}
-
-			return interaction.editReply({ embeds: [statusEmbed] });
-		} catch (e) {
-			await LOGGER.error(e, 'Failed to get server status');
-			return interaction.editReply('Failed to get server status.');
-		}
+		await handler.handleStatus({
+			serverChoice: args.getString('server', true) as ServerChoice,
+		});
 	},
 });
 
+class StatusCommandHandler extends BaseKiwiCommandHandler {
+	public async handleStatus(args: { serverChoice: ServerChoice }) {
+		const serverState = await this.getServerState(args);
+
+		if (!serverState) {
+			return;
+		}
+
+		if (serverState !== 'running') {
+			await this.interaction.editReply(`Server is currently ${serverState}!`);
+			return;
+		}
+
+		const mcStatus = await MCStatus.queryFull(args.serverChoice).catch(async (e) => {
+			await LOGGER.error(e, `Failed to get status for ${args.serverChoice}`);
+			return null;
+		});
+
+		if (!mcStatus) {
+			await this.interaction.editReply(`Failed to get status for ${args.serverChoice}!`);
+			return;
+		}
+
+		const serverMetrics = await this.getServerMetrics(args).catch(async (e) => {
+			await LOGGER.error(e, `Failed to get metrics for ${args.serverChoice}`);
+			return null;
+		});
+
+		if (!serverMetrics) {
+			await this.interaction.editReply(`Failed to get metrics for ${args.serverChoice}!`);
+			return;
+		}
+
+		const playerlist =
+			serverMetrics.players.playerList.length > 0
+				? serverMetrics.players.playerList.join('\n')
+				: 'There is currently nobody online.';
+
+		const statusEmbed = new KoalaEmbedBuilder(this.interaction.user, {
+			title: `${this.guild.name} ${args.serverChoice}`,
+			color: config.embedColors.green,
+			fields: [
+				{ name: 'Status', value: 'Online' },
+				{ name: 'Version', value: `${mcStatus.version?.name_clean ?? 'no version found'}` },
+				{
+					name: 'Performance',
+					value: `**${serverMetrics.performance.mspt}** MSPT | **${serverMetrics.performance.tps}** TPS`,
+				},
+				{
+					name: 'Hostile Mobcaps',
+					value: `Overworld: ${serverMetrics.mobcaps.overworld}\nThe Nether: ${serverMetrics.mobcaps.the_nether}\nThe End: ${serverMetrics.mobcaps.the_end}`,
+				},
+				{
+					name: 'Playercount',
+					value: `online: **${serverMetrics.players.count}** | max: **${serverMetrics.players.max}**`,
+				},
+				{
+					name: 'Playerlist',
+					value: playerlist,
+				},
+			],
+		});
+
+		const { mspt } = serverMetrics.performance;
+
+		if (mspt >= 30 && mspt < 40) {
+			statusEmbed.setColor(config.embedColors.yellow);
+		} else if (mspt >= 40 && mspt < 50) {
+			statusEmbed.setColor(config.embedColors.orange);
+		} else if (mspt >= 50) {
+			statusEmbed.setColor(config.embedColors.red);
+		}
+
+		if (this.guild.iconURL()) {
+			statusEmbed.setThumbnail(this.guild.iconURL());
+		}
+
+		await this.interaction.editReply({ embeds: [statusEmbed] });
+	}
+
+	/**
+	 * Get the server state of a server.
+	 * Returns the server state or null if the request failed.
+	 * @sideeffect Logs errors.
+	 */
+	private async getServerState(options: {
+		serverChoice: ServerChoice;
+	}): Promise<ServerState | null> {
+		const response = await ptero.servers
+			.getResourceUsage(config.mcConfig[options.serverChoice].serverId)
+			.catch(async (e) => {
+				await LOGGER.error(e, `Failed to get resource usage for ${options.serverChoice}`);
+				return null;
+			});
+
+		if (!response) {
+			return null;
+		}
+
+		return response.current_state;
+	}
+
+	/**
+	 * Get the server metrics of a server.
+	 * Returns the server metrics or null if the request failed.
+	 * @sideeffect Logs errors.
+	 */
+	private async getServerMetrics(options: {
+		serverChoice: ServerChoice;
+	}): Promise<ServerMetrics | null> {
+		const commands: string[] = [];
+
+		for (const dimension of MINECRAFT_DIMENSIONS) {
+			commands.push(`execute in minecraft:${dimension} run script run get_mob_counts('monster')`);
+		}
+
+		commands.push("script run reduce(system_info('server_last_tick_times'), _a+_, 0)/100", 'list');
+
+		const response = await RCONUtil.runMultipleCommands(options.serverChoice, commands).catch(
+			async (e) => {
+				await LOGGER.error(e, `Failed to get server metrics for ${options.serverChoice}`);
+				return null;
+			},
+		);
+
+		if (!response) {
+			return null;
+		}
+
+		const listResponse = response.pop();
+		const performanceResponse = response.pop();
+		const mobcapResponse = response;
+
+		if (!listResponse) {
+			await LOGGER.error(new Error(`Failed to get list response for ${options.serverChoice}`));
+			return null;
+		}
+
+		if (!performanceResponse) {
+			await LOGGER.error(
+				new Error(`Failed to get performance response for ${options.serverChoice}`),
+			);
+			return null;
+		}
+
+		if (mobcapResponse.length !== 3) {
+			await LOGGER.error(new Error(`Failed to get mobcap responses for ${options.serverChoice}`));
+			return null;
+		}
+
+		const performance = this.parsePerformance(performanceResponse);
+
+		if (!performance) {
+			await LOGGER.error(
+				new Error(`Failed to parse performance response for ${options.serverChoice}`),
+			);
+			return null;
+		}
+
+		const mobcaps = this.parseMobcaps(mobcapResponse);
+
+		if (!mobcaps) {
+			await LOGGER.error(new Error(`Failed to parse mobcap responses for ${options.serverChoice}`));
+			return null;
+		}
+
+		const players = this.getOnlinePlayers(listResponse);
+
+		if (!players) {
+			await LOGGER.error(new Error(`Failed to parse online players for ${options.serverChoice}`));
+			return null;
+		}
+
+		return {
+			performance,
+			mobcaps,
+			players,
+		};
+	}
+
+	private parsePerformance(rconResponse: string): ServerPerformance | null {
+		const splitNumbers = rconResponse.split(' ')[2];
+
+		if (!splitNumbers) {
+			return null;
+		}
+
+		const mspt = Math.round(Number.parseFloat(splitNumbers) * 100) / 100;
+
+		let tps: number;
+
+		if (mspt <= 50) {
+			tps = 20;
+		} else {
+			tps = Math.round((1000 / mspt) * 10) / 10;
+		}
+
+		return { mspt, tps };
+	}
+
+	private parseMobcaps(mobcapResponse: string[]): DimensionMobcaps | null {
+		const replaced: string[] = [];
+
+		for (const res of mobcapResponse) {
+			replaced.push(res.replace(/^.{0,3}| \(.*\)|[[\]]/g, '').replace(/, /g, ' | '));
+		}
+
+		if (!replaced[0] || !replaced[1] || !replaced[2]) {
+			return null;
+		}
+
+		return {
+			overworld: replaced[0],
+			the_nether: replaced[1],
+			the_end: replaced[2],
+		};
+	}
+
+	private getOnlinePlayers(listResponse: string): OnlinePlayers | null {
+		const splitWords = listResponse.split(' ');
+
+		if (splitWords.length < 8 || !splitWords[2] || !splitWords[7]) {
+			return null;
+		}
+
+		try {
+			const count = Number.parseInt(splitWords[2]);
+			const max = Number.parseInt(splitWords[7]);
+
+			let playerList: string[] = [];
+
+			if (count > 0) {
+				const splitResponse = listResponse.split(': ')[1]?.split(', ');
+
+				if (!splitResponse) {
+					return null;
+				}
+
+				playerList = splitResponse;
+			}
+
+			return {
+				count,
+				max,
+				playerList,
+			};
+		} catch {
+			return null;
+		}
+	}
+}
+
 const MINECRAFT_DIMENSIONS = ['overworld', 'the_nether', 'the_end'] as const;
+type ServerState = 'starting' | 'running' | 'stopping' | 'offline';
+
 type MinecraftDimension = (typeof MINECRAFT_DIMENSIONS)[number];
 type DimensionMobcaps = {
 	[K in MinecraftDimension]: string;
@@ -114,105 +308,3 @@ type ServerMetrics = {
 	performance: ServerPerformance;
 	players: OnlinePlayers;
 };
-
-async function getServerMetrics(server: ServerChoice): Promise<ServerMetrics> {
-	const commands: string[] = [];
-
-	for (const dimension of MINECRAFT_DIMENSIONS) {
-		commands.push(`execute in minecraft:${dimension} run script run get_mob_counts('monster')`);
-	}
-
-	commands.push(`script run reduce(system_info('server_last_tick_times'), _a+_, 0)/100`);
-
-	commands.push('list');
-
-	const response = await RCONUtil.runMultipleCommands(server, commands);
-
-	const listResponse = response.pop();
-	const performanceResponse = response.pop();
-	const mobcapResponse = response;
-
-	if (!listResponse) {
-		throw new Error(`Failed to query the playerlist for ${server}`);
-	}
-
-	if (!performanceResponse) {
-		throw new Error(`Failed to query server performance for ${server}`);
-	}
-
-	return {
-		mobcaps: getMobcaps(mobcapResponse),
-		performance: getPerformance(performanceResponse),
-		players: getOnlinePlayers(listResponse),
-	};
-}
-
-function getPerformance(rconResponse: string): ServerPerformance {
-	const splitNumbers = rconResponse.split(' ')[2];
-
-	if (!splitNumbers) {
-		throw new Error('Failed to parse server data');
-	}
-
-	const mspt = Math.round(Number.parseFloat(splitNumbers) * 100) / 100;
-
-	let tps: number;
-
-	if (mspt <= 50) {
-		tps = 20;
-	} else {
-		tps = Math.round((1000 / mspt) * 10) / 10;
-	}
-
-	return { mspt, tps };
-}
-
-function getMobcaps(rconResponses: string[]): DimensionMobcaps {
-	if (rconResponses.length !== 3) {
-		throw new Error('Failed to parse mobcaps because of unexpected server response data');
-	}
-
-	const replaced = [];
-
-	for (const res of rconResponses) {
-		replaced.push(res.replace(/^.{0,3}| \(.*\)|[[\]]/g, '').replace(/, /g, ' | '));
-	}
-
-	if (!replaced[0] || !replaced[1] || !replaced[2]) {
-		throw new Error('Failed to parse mobcaps because of unexpected server response data');
-	}
-
-	return {
-		overworld: replaced[0],
-		the_nether: replaced[1],
-		the_end: replaced[2],
-	};
-}
-
-function getOnlinePlayers(listResponse: string): OnlinePlayers {
-	const splitWords = listResponse.split(' ');
-
-	if (splitWords.length < 8 || !splitWords[2] || !splitWords[7]) {
-		throw new Error('Failed to parse playerlist because of unexpected server response data');
-	}
-
-	const count = Number.parseInt(splitWords[2]);
-	const max = Number.parseInt(splitWords[7]);
-	let playerList: string[] = [];
-
-	if (count > 0) {
-		const splitResponse = listResponse.split(': ')[1]?.split(', ');
-
-		if (!splitResponse) {
-			throw new Error('Failed to parse playerlist because of unexpected server response data');
-		}
-
-		playerList = splitResponse;
-	}
-
-	return {
-		count,
-		max,
-		playerList,
-	};
-}
