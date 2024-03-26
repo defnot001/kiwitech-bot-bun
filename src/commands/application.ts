d984263497354f0a9ba4baff6fd919d77c458f32
defnot001 <marc.miller994@gmail.com>
2024-03-26 11:40:37 +0100

    no idea what's going on here

--- src/commands/application.ts
+++ src/commands/application.ts
@@ -902,6 +902,9 @@ class ApplicationCommandHandler extends BaseKiwiCommandHandler {
 		}
 
 		const footer = message.embeds[0].footer.text;
+
+		console.debug(footer);
+
 		const split = footer.split('|');
 
 		if (!split[1]) {
